5a472950983e06fed9a2af4fc5cb2f45ab1d965b
Doev Lopper <happyman@hotmail.fr>
2018-06-28 22:48:09 +0200

    This Is Why We Don't Push To Production On Fridays

--- src/main/cpp/cfs/com/Error.cpp
+++ src/main/cpp/cfs/com/Error.cpp
@@ -1,24 +1,88 @@
 
 #include <cfs/com/Error.hpp>
-///
-/// Convienience functions for making system_error exception
-///
-inline std::system_error make_syserr(int e, const char * msg)
+
+using namespace cfs::com;
+
+Error::Error()
+{
+
+}
+
+Error::~Error()
+{
+
+}
+
+const char* Error::name() const noexcept 
+{
+  return "Error: ";
+}
+
+std::string Error::message(int ev) const 
+{
+    switch (static_cast<cfs::com::DdsErrs>(ev)) 
+    {  
+  case DdsErrs::None:
+      return std::string("connection refuse");
+  break;
+
+        default:
+            return std::string("[unrecognized error]");
+  break;
+    }
+}
+
+std::error_condition Error::default_error_condition(int ev) const noexcept
+{
+    switch (static_cast<cfs::com::DdsErrs>(ev))  
+    {
+        case DdsErrs::None:
+            //return SubsystemError::SubsysInternal;
+        default:
+            return {};
+    }
+}
+
+bool Error::equivalent(const std::error_code& code,
+      int condition) const noexcept 
+{
+    switch (static_cast<Severity>(condition))
+    {
+//       case Severity::Heisenbug:
+//	    return ((code == SubsystemError::SubsysInternal)
+//		    || (code == SubsystemError::SubsysInternal));
+//       case Severity::Hindenbug
+//       case Severity::Schroedinbug
+//       case Severity::Bohrbug
+//       case Severity::Mandelbug
+       default:
+           return (false);
+    }
+}
+
+std::error_code Error::make_error_code(cfs::com::DdsErrs e)
+{
+    return {static_cast<int>(e), errorCategory};
+}
+
+std::system_error Error::make_syserr(int e, const char * msg)
 {
     return std::system_error(std::error_code(e, std::system_category()), msg);
 }
 
-inline std::system_error make_syserr(int e, const std::string & msg)
+std::system_error Error::make_syserr(int e, const std::string & msg)
 { 
     return make_syserr(e, msg.c_str()); 
 }
 
-inline std::system_error make_syserr(const std::string & msg)
+std::system_error Error::make_syserr(const std::string & msg)
 { 
     return make_syserr(errno, msg); 
 }
 
-inline std::system_error make_syserr(const char * msg)
+std::system_error Error::make_syserr(const char * msg)
 { 
     return make_syserr(errno, msg); 
 }
+
+
--- src/main/cpp/cfs/com/Error.hpp
+++ src/main/cpp/cfs/com/Error.hpp
@@ -9,14 +9,34 @@
 
 namespace cfs::com
 {
-    class Error
+
+    enum class DdsErrs 
+    {
+        None, /* ... */ 
+    };
+
+    enum class SubsystemError
+    {
+  SubsysInternal,
+    };
+
+    enum class Severity
+    {
+        Heisenbug,    //!< Bug that seems to disappear or alter its behavior when one attempts to study it.
+        Hindenbug,    //!< Bug with catastrophic behavior.
+        Schroedinbug, //!< Bug that manifests itself in running software.
+        Bohrbug,      //!< Bug that do not change its behavior and are relatively easily detected.
+        Mandelbug     //!< Bug whose causes are so complex.
+    };
+
+    class Error  : std::error_category
     {
 
     public:
 
         Error(void);
 //        Error(std::string domain, std::uint32_t code, Dictionary userInfo);
-        ~Error(void);
+        virtual ~Error(void);
         std::uint32_t code( void ) const;
         std::string domain( void ) const;
 //        cfs::com::Dictionary  userInfo( void ) const;
@@ -24,12 +44,43 @@ namespace cfs::com
         std::string localizedRecoveryOptions( void ) const;
         std::string localizedRecoverySuggestion( void ) const;
         std::string localizedFailureReason( void ) const;
-
-
+//        virtual Error * clone() const = 0;
     protected:
+        const char* name() const noexcept override;
+        std::string message(int ev) const override;
+        std::error_condition default_error_condition(int ev) const noexcept override;
+        bool equivalent(const std::error_code& code, int condition) const noexcept override;
     private:
+
+        ///
+        /// Convienience functions for making system_error exception
+        ///
+
+        std::error_code make_error_code(cfs::com::DdsErrs);
+        std::error_condition make_error_condition(cfs::com::SubsystemError e);
+        std::error_condition make_error_condition(cfs::com::Severity e);
+        std::system_error make_syserr(int e, const char * msg);
+        std::system_error make_syserr(int e, const std::string & msg);
+        std::system_error make_syserr(const std::string & msg);
+        std::system_error make_syserr(const char * msg);
     };
 
+
+    const Error errorCategory {};
+}
+
+
+namespace std 
+{
+    template<> 
+    struct is_error_condition_enum<cfs::com::DdsErrs> : std::true_type
+    {};
+    template <>
+    struct is_error_condition_enum<cfs::com::SubsystemError> : std::true_type 
+    {};
+    template <>
+    struct is_error_condition_enum<cfs::com::Severity> : std::true_type
+    {};
 }
 
 #endif
