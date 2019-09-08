baac335d2b37741ed3bb1af4020df8e95c4ae86d
Adrien .H <happyman@hotmail.fr>
2019-09-08 19:57:51 +0200

    [WIP] Run all in once. Makefile commented and no goals

--- src/main/resources/docker/makefiles/common-goals.mk
+++ src/main/resources/docker/makefiles/common-goals.mk
@@ -86,7 +86,7 @@ build: build-image dtr-login push dtr-logout ## Build and deploy Docker images b
 .PHONY: build-image
 build-image:
 	$(Q)echo "$(SH_CYAN) Build of $(BUILDER_FQIN) from $(BASE_IMAGE) $(SH_DEFAULT)"
-	$(Q)$(DOCKER) build $(DOCKER_LABEL) $(BUILD_ARGS) --tag  $(BUILDER_FQIN):$(SEM_VERSION) --file Dockerfile . 2>&1 | tee $(GIT_ROOTDIR)/$(shell basename $(CURDIR))_build_output.log
+	# $(Q)$(DOCKER) build $(DOCKER_LABEL) $(BUILD_ARGS) --tag  $(BUILDER_FQIN):$(SEM_VERSION) --file Dockerfile . 2>&1 | tee $(GIT_ROOTDIR)/$(shell basename $(CURDIR))_build_output.log
 	$(Q)echo "Build of $(BUILDER_FQIN):$(SEM_VERSION) finished."
 
 .PHONY: push
@@ -95,12 +95,12 @@ build-image:
 .PHONY: push-image
 push-image:
 	$(Q)echo "$(SH_BLUE) Apply tag [$(SEM_VERSION)|latest] on $(BUILDER_FQIN)  $(SH_DEFAULT)"
-	$(Q)$(DOCKER) tag $(BUILDER_FQIN):$(SEM_VERSION) $(BUILDER_FQIN):latest
+	# $(Q)$(DOCKER) tag $(BUILDER_FQIN):$(SEM_VERSION) $(BUILDER_FQIN):latest
 	$(Q)echo
 ifneq ($(CI_RUNNER_TAGS),)
 	$(Q)echo "$(SH_BLUE) Pushing $(BUILDER_FQIN):[$(SEM_VERSION)|latest] to $(DOCKER_TRUSTED_REGISTRY)$(SH_DEFAULT)"
-	$(Q)$(DOCKER) push $(BUILDER_FQIN):$(SEM_VERSION)
-	$(Q)$(DOCKER) push $(BUILDER_FQIN):latest
+	# $(Q)$(DOCKER) push $(BUILDER_FQIN):$(SEM_VERSION)
+	# $(Q)$(DOCKER) push $(BUILDER_FQIN):latest
 	# $(Q)echo "$(SEM_VERSION)" > $(VERSIONFILE)
 	$(Q)echo "$(SH_GREEN) Images $(BUILDER_FQIN):[$(SEM_VERSION)|latest] pushed to DTR$(SH_DEFAULT)"
 endif
